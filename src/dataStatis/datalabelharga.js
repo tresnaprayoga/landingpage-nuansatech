d78370605f8ff25e60ab87822a67e5727cc9b2d2
tresnaprayoga <pujitresnaprayoga@gmail.com>
2025-09-01 10:56:05 +0700

    update harga

--- src/dataStatis/datalabelharga.js
+++ src/dataStatis/datalabelharga.js
@@ -1,7 +1,8 @@
 const pricingPlans = [
-  { name: '1 Tahun Berlangganan', price: 'IDR 1000.000', duration: 'Sudah termasuk harga Hosting dan Template Web' },
-  { name: '2 Tahun Berlangganan', price: 'IDR 1700.000', duration: 'Sudah termasuk harga Hosting dan Template Web' },
-  { name: '3 Tahun Berlangganan', price: 'IDR 2500.000', duration: 'Sudah termasuk harga Hosting dan Template Web' },
+  { name: '1 Tahun Berlangganan', price: 'IDR 500.000', duration: 'Sudah termasuk harga Hosting, Template Web statis dan update content max 5 kali' },
+  { name: '2 Tahun Berlangganan', price: 'IDR 1000.000', duration: 'Sudah termasuk harga Hosting, Template Web statis dan update content max 5 kali' },
+  { name: '3 Tahun Berlangganan', price: 'IDR 1500.000', duration: 'Sudah termasuk harga Hosting, Template Web statis dan update content max 5 kali' },
+  { name: 'Website custom sesuai permintaan', price: 'IDR 3000.000 - 5000.000', duration: 'Harga  Disesuaikan dengan kompleksitas fitur yang dibutuhkan' },
 ];
 
 export default pricingPlans;
