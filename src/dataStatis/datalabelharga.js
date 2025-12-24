d7f461d113d68a49e2a7330d644e81a966552a9d
tresnaprayoga <pujitresnaprayoga@gmail.com>
2025-12-24 13:41:06 +0700

    edit warna

--- src/dataStatis/datalabelharga.js
+++ src/dataStatis/datalabelharga.js
@@ -3,19 +3,19 @@ const pricingPlans = [
     name: '1 Tahun Berlangganan',
     price: 'IDR 500.000',
     duration:
-      ' ✔️ Gratis Hosting & Domain(xyz, .my.id), \n ✔️Template Web statis \n  ✔️update content max 5 kali (via tim kami) \n ✔️Fitur: Formulir /WhatsApp, Google Map ✔️Desain responsif (Smart Phone & Desktop ) \n✔️ Revisi: 2x revisi minor (teks/gambar) \n✔️Gratis Copywriting Konten \n ✔️Integrasi Medsos  \n✔️Waktu Pengerjaan: 2 Hari kerja',
+      ' ✔️ Gratis Hosting & Domain(xyz, .my.id), \n ✔️Template Web statis \n  ✔️update content max 5 kali (via tim kami) \n ✔️Fitur: Formulir /WhatsApp, Google Map \n ✔️Desain responsif (Smart Phone & Desktop ) \n✔️ Revisi: 2x revisi minor (teks/gambar) \n✔️Gratis Copywriting Konten \n ✔️Integrasi Medsos  \n✔️Waktu Pengerjaan: 2 Hari kerja',
   },
   {
     name: '2 Tahun Berlangganan',
     price: 'IDR 1000.000',
     duration:
-      ' ✔️ Gratis Hosting & Domain (xyz, .my.id), \n ✔️Template Web statis \n  ✔️update content max 10 kali (via tim kami) \n ✔️ Fitur: Formulir /WhatsApp, Google Map ✔️Desain responsif (Smart Phone & Desktop) \n✔️ Revisi: 2x revisi minor (teks/gambar) \n✔️Gratis Copywriting Konten \n ✔️Integrasi Medsos \n✔️Waktu Pengerjaan: 2 Hari kerja',
+      ' ✔️ Gratis Hosting & Domain (xyz, .my.id), \n ✔️Template Web statis \n  ✔️update content max 10 kali (via tim kami) \n ✔️ Fitur: Formulir /WhatsApp, Google Map \n✔️Desain responsif (Smart Phone & Desktop) \n✔️ Revisi: 5x revisi minor (teks/gambar) \n✔️Gratis Copywriting Konten \n ✔️Integrasi Medsos \n✔️Waktu Pengerjaan: 2 Hari kerja',
   },
   {
     name: '3 Tahun Berlangganan',
     price: 'IDR 1500.000',
     duration:
-      ' ✔️ Gratis Hosting & Domain (xyz, .my.id), \n ✔️Template Web statis \n  ✔️update content max 12 kali (via tim kami) \n ✔️Fitur: Formulir /WhatsApp, Google Map ✔️Desain responsif (Smart Phone & Desktop) \n✔️ Revisi: 2x revisi minor (teks/gambar) \n✔️Gratis Copywriting Konten \n ✔️Integrasi Medsos \n✔️Waktu Pengerjaan: 2 Hari kerja',
+      ' ✔️ Gratis Hosting & Domain (xyz, .my.id), \n ✔️Template Web statis \n  ✔️update content max 12 kali (via tim kami) \n ✔️Fitur: Formulir /WhatsApp, Google Map \n✔️Desain responsif (Smart Phone & Desktop) \n✔️ Revisi: 7x revisi minor (teks/gambar) \n✔️Gratis Copywriting Konten \n ✔️Integrasi Medsos \n✔️Waktu Pengerjaan: 2 Hari kerja',
   },
   // { name: 'Website custom sesuai permintaan', price: 'IDR 3000.000 - 5000.000', duration: 'Harga  Disesuaikan dengan kompleksitas fitur yang dibutuhkan' },
 ];
